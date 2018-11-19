f749ef0e11b380df6fbc2331a2796f62d9a3a57f
JaretWright <hkkA223>
2018-11-19 16:36:36 -0500

    Added validation to the Student class

--- src/Student.java
+++ src/Student.java
@@ -15,7 +15,34 @@ public class Student {
         grades = new ArrayList<>();
     }
 
-    
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        if (firstName.isEmpty())
+            throw new IllegalArgumentException("first name cannot be empty");
+        this.firstName = firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName.isEmpty())
+            throw new IllegalArgumentException("last name cannot be empty");
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Float> getGrades() {
+        return grades;
+    }
+
+    public void addGrade(float newGrade) {
+        if (newGrade >=0 && newGrade <=100)
+            grades.add(newGrade);
+        else
+            throw new IllegalArgumentException("grade must be 0-100");
+    }
 }
